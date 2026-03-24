a06588056071b48169a1504ef6e6d67b189b8e07
Dakhama mehdi <49924401+dakhama-mehdi@users.noreply.github.com>
2026-03-24 15:33:10 +0100

    Add files via upload

---
+++ Scripts/Find-BruteForce.ps1
@@ -0,0 +1,84 @@
+﻿
+<#
+.SYNOPSIS
+Detects potential brute force attempts from Windows Security logs.
+
+.DESCRIPTION
+This script analyzes failed logon events (Event ID 4625) over a defined time window.
+It extracts key information such as username, source IP address, timestamp, and failure reason.
+The goal is to quickly identify suspicious authentication activity.
+
+.PARAMETER Hours
+Defines the time window (in hours) to search for failed logon events.
+
+.EXAMPLE
+.\Find-BruteForce.ps1 -Hours 10
+
+.NOTES
+Author: Mehdi Dakhama
+Status: Work in progress
+#>
+
+# Time in hours
+$hours = 10
+
+# Convert to milliseconds
+$ms = [int](New-TimeSpan -Hours $hours).TotalMilliseconds
+
+# Error code mapping
+$StatusMap = @{
+    "0xc0000064" = "User does not exist"
+    "0xc000006a" = "Wrong password"
+    "0xc000006d" = "Bad username or password"
+    "0xc000006e" = "Account restriction"
+    "0xc000006f" = "Invalid logon hours"
+    "0xc0000070" = "Invalid workstation"
+    "0xc0000071" = "Password expired"
+    "0xc0000072" = "Account disabled"
+    "0xc0000193" = "Account expired"
+    "0xc0000234" = "Account locked out"
+}
+
+# Resolve function
+function Resolve-Status {
+    param($Code)
+
+    if ($StatusMap.ContainsKey($Code)) {
+        return $StatusMap[$Code]
+    }
+    else {
+        return $Code
+    }
+}
+
+# XPath filter
+$xpath = "*[System[(EventID=4625) and TimeCreated[timediff(@SystemTime) <= $ms]]] and *[EventData[Data[@Name='LogonType']='3']]"
+
+# Retrieve events
+$result = Get-WinEvent -FilterXPath $xpath -LogName Security | ForEach-Object {
+
+    $Event = ([xml]$_.ToXml()).Event  
+
+    # Convert EventData to hashtable
+    $data = @{}
+    $Event.EventData.Data | ForEach-Object {
+        $data[$_.Name] = $_.'#text'
+    }
+
+    $obj = [PSCustomObject]$data
+
+    # Final object
+    [PSCustomObject]@{
+        UserName    = $obj.TargetUserName
+        IpAddress   = $obj.IpAddress
+        Date        = [datetime]$Event.System.TimeCreated.SystemTime
+        Reason      = Resolve-Status $obj.SubStatus
+        SubStatus   = $obj.SubStatus
+        #ProcessName = $obj.LogonProcessName
+        #Protocol    = $obj.AuthenticationPackageName
+        #Status      = $obj.Status
+    }
+}
+
+# Display
+$result | Format-Table -AutoSize
